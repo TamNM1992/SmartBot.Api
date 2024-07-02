77717262f249acb9bff68634c7ff6b9eb6a13f8f
Admin <Admin@DESKTOP-48JPN0S>
2024-07-02 16:07:44 +0700

    fix history log action

--- SmartBot.DataDto/Action/LogActionDto.cs
+++ SmartBot.DataDto/Action/LogActionDto.cs
@@ -2,15 +2,26 @@
 
 namespace SmartBot.DataDto.Action
 {
+    public class LogScriptDto
+    {
+        public string ScriptName { get; set; }
+        public List<LogActionDto> ListLogAction { get; set; }
+        
+    }
     public class LogActionDto
     {
-        public int IdUser { get; set; }
-        public int IdFb { get; set; }
-        public string? NameFb { get; set; }
-        public string Action { get; set; }
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
         public DateTime StartTime { get; set; }
+
         public DateTime EndTime { get; set; }
+
+        public int IdFb { get; set; }
+        public string NameFb { get; set; } = null!;
+
         public bool Result { get; set; }
-        public string ResultDetail { get; set; } = string.Empty;
+        public List<string> ListLogStep { get; set; }
     }
 }
--- SmartBot.Services/Actions/ActionsService.cs
+++ SmartBot.Services/Actions/ActionsService.cs
@@ -1,10 +1,15 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Graph.Models;
 using SmartBot.Common.Enums;
 using SmartBot.Common.Helpers;
 using SmartBot.DataAccess.Entities;
 using SmartBot.DataAccess.Interface;
 using SmartBot.DataDto.Action;
 using SmartBot.DataDto.Base;
+using SmartBot.DataDto.Script;
+using System.Runtime.CompilerServices;
+using static Azure.Core.HttpHeader;
 using User = SmartBot.DataAccess.Entities.User;
 
 namespace SmartBot.Services.Action
@@ -17,14 +22,24 @@ namespace SmartBot.Services.Action
         private readonly ICommonRepository<DataAccess.Entities.User> _userRepository;
         private readonly ICommonRepository<UsersAccountFb> _userAccountRepository;
         private readonly ICommonRepository<AccountFb> _accountRepository;
+        private readonly ICommonRepository<LogActionScript> _logActionRepository;
+        private readonly ICommonRepository<LogStepAction> _LogStepRepository;
 
-        public ActionsService(IMapper mapper, ICommonUoW commonUoW, ICommonRepository<User> userRepository, ICommonRepository<UsersAccountFb> userAccountRepository, ICommonRepository<AccountFb> accountRepository)
+        public ActionsService(IMapper mapper, ICommonUoW commonUoW, ICommonRepository<User> userRepository, ICommonRepository<UsersAccountFb> userAccountRepository, ICommonRepository<AccountFb> accountRepository, ICommonRepository<LogActionScript> logActionRepository,
+            ICommonRepository<LogStepAction> LogStepRepository)
         {
             _mapper = mapper;
             _commonUoW = commonUoW;
             _userRepository = userRepository;
             _userAccountRepository = userAccountRepository;
             _accountRepository = accountRepository;
+            _logActionRepository=logActionRepository;
+            _LogStepRepository=LogStepRepository;
+        }
+
+        private List<string> Test(int idAction)
+        {
+            return _LogStepRepository.FindAll(y => y.IdLogAction== idAction).Select(x => x.StepDetail).ToList();
         }
 
         public ResponseBase GetActionHistory(string token, DateTime? start, DateTime? end, int? idFb, int? actionId)
@@ -32,59 +47,35 @@ namespace SmartBot.Services.Action
             ResponseBase response = new ResponseBase();
             try
             {
-                // Tượng trưng cho bảng Actions
-                Dictionary<int, string> actions = new Dictionary<int, string>();
-                actions.Add(1, "Thích");
-                actions.Add(2, "Bình luận");
-                actions.Add(3, "Đăng bài");
-                actions.Add(4, "Chia sẻ");
-                actions.Add(5, "Thả tim");
+                var idUser = int.Parse(Token.Authentication(token));
+                var logAc = _logActionRepository.FindAll(x => x.IdUser == idUser)
+                                                .Include(x => x.IdScriptNavigation)
+                                                .Include(x => x.LogStepActions);
 
-                // Tượng trưng cho bảng LogActions
-                List<LogActionDto> logActions = new List<LogActionDto>()
+
+                var data = new LogScriptDto
                 {
-                    new LogActionDto
+                    ScriptName = logAc.FirstOrDefault().IdScriptNavigation.Name,
+                    ListLogAction = logAc.Select(x => new LogActionDto
                     {
-                        IdUser = 2,
-                        StartTime = DateTime.Parse("2024-06-19"),
-                        EndTime = DateTime.Parse("2024-06-19"),
-                        IdFb = 3,
-                        Action = actions[1],
-                        NameFb = "quanlhjos@gmail.com",
-                        ResultDetail = "Thành công",
-                        Result = true
-                    },
-                    new LogActionDto
-                    {
-                        IdUser = 2,
-                        StartTime = DateTime.Parse("2024-06-19"),
-                        EndTime = DateTime.Parse("2024-06-19"),
-                        IdFb = 4,
-                        Action = actions[4],
-                        NameFb = "0961082002",
-                        ResultDetail = "Thất bại",
-                        Result = false
-                    },
-                    new LogActionDto
-                    {
-                        IdUser = 11,
-                        StartTime = DateTime.Parse("2024-07-20"),
-                        EndTime = DateTime.Parse("2024-07-20"),
-                        IdFb = 4,
-                        Action = actions[5],
-                        NameFb = "0961082002",
-                        ResultDetail = "Thất bại",
-                        Result = false
-                    }
+                        Id = x.Id,
+                        Name = x.Name,
+                        StartTime = x.StartTime,
+                        EndTime = x.EndTime,
+                        IdFb = x.IdFb,
+                        NameFb = x.NameFb,
+                        Result = x.Result,
+                        ListLogStep = x.LogStepActions.Select(x => x.StepDetail).ToList()
+                    }).ToList()
                 };
 
-                var idUser = int.Parse(Token.Authentication(token));
-                var data = logActions.Where(log =>
-                           log.IdUser == idUser &&
-                           (!start.HasValue || log.StartTime >= start.Value) &&
-                           (!end.HasValue || log.EndTime <= end.Value) &&
-                           (!idFb.HasValue || log.IdFb == idFb) &&
-                           (!actionId.HasValue || log.Action == actions[(int)actionId])).ToList();
+
+                //var data = logActions.Where(log =>
+                //           log.IdUser == idUser &&
+                //           (!start.HasValue || log.StartTime >= start.Value) &&
+                //           (!end.HasValue || log.EndTime <= end.Value) &&
+                //           (!idFb.HasValue || log.IdFb == idFb) &&
+                //           (!actionId.HasValue || log.Action == actions[(int)actionId])).ToList();
 
                 response.Data = data;
                 return response;
